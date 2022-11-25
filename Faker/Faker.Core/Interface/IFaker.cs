c25e403c227e9eaf571497742f1dfa9fc3a3d6ce
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-11-25 18:36:26 +0300

    New features
    Added:
    - Faker.DebudSession for console testing (for personal use only);
    - IFaker interface which provides Faker functionality to other elements.
    
    Cosmetic:
    - Deleted unnecessary usings;
    - File-scoped namespaces

---
+++ Faker/Faker.Core/Interface/IFaker.cs
@@ -0,0 +1,10 @@
+﻿namespace Faker.Core.Interface;
+
+// Provide basic Faker functionality for use in different places
+public interface IFaker
+{
+    T Create<T>();
+
+    object Create(Type t);
+
+}
