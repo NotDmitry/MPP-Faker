76d35d8b9259d8c4f05984dd1b5b59367970e7ec
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-05 17:04:24 +0300

    New feaature: IntGenerator
    Added class IntGenerator for creating random signed Int32 values

---
+++ Faker/Faker.Core/TypeGenerators/IntGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Faker.Core.Interface;
+using Faker.Core.Other;
+
+namespace Faker.Core.TypeGenerators;
+
+public class IntGenerator : IValueGenerator
+{
+    public bool CanGenerate(Type type) => type == typeof(int);
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        return context.Random.Next(int.MinValue, int.MaxValue);
+    }
+}
