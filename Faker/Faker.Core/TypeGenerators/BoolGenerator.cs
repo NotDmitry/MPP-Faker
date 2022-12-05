4b86a77711eda2ad1fddb213210142e995724da6
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-05 16:34:44 +0300

    New feature: BoolGenerator
    Added class BoolGenerator for creating random boolean value

---
+++ Faker/Faker.Core/TypeGenerators/BoolGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Faker.Core.Interface;
+using Faker.Core.Other;
+
+namespace Faker.Core.TypeGenerators;
+
+public class BoolGenerator : IValueGenerator
+{
+    public bool CanGenerate(Type type) => type == typeof(bool);
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        return context.Random.Next(2) == 1;
+    }
+}
