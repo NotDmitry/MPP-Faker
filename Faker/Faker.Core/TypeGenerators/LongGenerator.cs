813fafdf48b17c1149f6349ccabc1f6e04db059a
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-05 17:39:03 +0300

    New feature: LongGenerator
    Added class LongGenerator for creating random signed Int64 values

---
+++ Faker/Faker.Core/TypeGenerators/LongGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Faker.Core.Interface;
+using Faker.Core.Other;
+
+namespace Faker.Core.TypeGenerators;
+
+public class LongGenerator : IValueGenerator
+{
+    public bool CanGenerate(Type type) => type == typeof(long);
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        return context.Random.NextInt64(long.MinValue, long.MaxValue);
+    }
+}
