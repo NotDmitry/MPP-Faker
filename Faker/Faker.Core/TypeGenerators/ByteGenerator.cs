98b3ec00bdce2d106de18c506091fe146849d258
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-05 17:47:07 +0300

    New feature: ByteGenerator
    Added class ByteGenerator for creating random byte values

---
+++ Faker/Faker.Core/TypeGenerators/ByteGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Faker.Core.Interface;
+using Faker.Core.Other;
+
+namespace Faker.Core.TypeGenerators;
+
+public class ByteGenerator : IValueGenerator
+{
+    public bool CanGenerate(Type type) => type == typeof(byte);
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        return (byte)context.Random.Next(byte.MinValue, byte.MaxValue);
+    }
+}
