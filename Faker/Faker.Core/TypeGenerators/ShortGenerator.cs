8e3198db0b3b75889fea9c5ac864588390dfade9
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-05 17:08:46 +0300

    New feaature: ShortGenerator
    Added class ShortGenerator for creating random signed Int16 values

---
+++ Faker/Faker.Core/TypeGenerators/ShortGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Faker.Core.Interface;
+using Faker.Core.Other;
+
+namespace Faker.Core.TypeGenerators;
+
+public class ShortGenerator : IValueGenerator
+{
+    public bool CanGenerate(Type type) => type == typeof(short);
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        return context.Random.Next(short.MinValue, short.MaxValue);
+    }
+}
