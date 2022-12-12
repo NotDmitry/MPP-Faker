4efdf16f4e7ea5272e31f59e1d960c95ee9ad2c2
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-12 19:51:34 +0300

    New feature: CharGenerator
    Added class CharGenerator for creating random symbols

---
+++ Faker/Faker.Core/TypeGenerators/CharGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Faker.Core.Interface;
+using Faker.Core.Other;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Core.TypeGenerators;
+
+public class CharGenerator : IValueGenerator
+{
+    private readonly string _perfectPangram = "Th;1e* q.ui2ck( bro) -3w>n4 +f5ox _6 =jumps7 o<ve,8r ?9a :l0azy 5dog " +
+        "B]OT6H& 7FI0^ CKLE% 8DW[A91 $RVES2} #JINX @MY3{ !PIG Q4UIZ ";
+
+    public bool CanGenerate(Type type) => type == typeof(char);
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        int position = context.Random.Next(0, _perfectPangram.Length);
+        return _perfectPangram[position];
+    }
+}
