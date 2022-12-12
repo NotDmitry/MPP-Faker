90118670a44acfc40ed91fe4f03f39af8ac2d06c
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-12 20:09:44 +0300

    New feature: FloatGenerator
    Added class FloatGenerator for creating random float values

--- Faker/Faker.Core/TypeGenerators/FloatGenerator.cs
+++ Faker/Faker.Core/TypeGenerators/FloatGenerator.cs
@@ -3,16 +3,16 @@ using Faker.Core.Other;
 
 namespace Faker.Core.TypeGenerators;
 
-public class DoubleGenerator : IValueGenerator
+public class FloatGenerator : IValueGenerator
 {
     private readonly double _max = 100.0;
 
     private readonly double _min = 0.0;
 
-    public bool CanGenerate(Type type) => type == typeof(double);
+    public bool CanGenerate(Type type) => type == typeof(float);
 
     public object Generate(Type typeToGenerate, GeneratorContext context)
     {
-        return context.Random.NextDouble() * (_max - _min) + _min;
+        return (float)(context.Random.NextDouble() * (_max - _min) + _min);
     }
 }
