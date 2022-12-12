a6b39f149144e7e1af539750764a7c68754ba1cb
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-13 00:55:22 +0300

    New feature: StringGenerator
    Added class StringGenerator for creating random text using pangrams

---
+++ Faker/Faker.Core/TypeGenerators/StringGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Faker.Core.Other;
+using System.Text;
+
+namespace Faker.Core.TypeGenerators;
+
+public class StringGenerator
+{
+    private readonly string _perfectPangram = "Th;1e* q.ui2ck( bro) -3w>n4 +f5ox _6 =jumps7 o<ve,8r ?9a :l0azy 5dog " +
+    "B]OT6H& 7FI0^ CKLE% 8DW[A91 $RVES2} #JINX @MY3{ !PIG Q4UIZ ";
+
+    public bool CanGenerate(Type type) => type == typeof(string);
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        int length = context.Random.Next(10, 51);
+        StringBuilder builder = new();
+
+        for (int i = 0; i < length; i++)
+            builder.Append(_perfectPangram[context.Random.Next(0, _perfectPangram.Length)]);
+        
+        return builder.ToString();
+    }
+}
