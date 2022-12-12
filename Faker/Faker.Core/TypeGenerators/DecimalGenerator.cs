34b16c7e63f2c7839d61b31ea2d96674096f29b5
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-13 00:29:30 +0300

    New feature: DecimalGenerator
    Added class DecimalGenerator for creating random decimal values

---
+++ Faker/Faker.Core/TypeGenerators/DecimalGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Faker.Core.Interface;
+using Faker.Core.Other;
+
+namespace Faker.Core.TypeGenerators;
+
+public class DecimalGenerator : IValueGenerator
+{
+    public bool CanGenerate(Type type) => type == typeof(decimal);
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        return context.Random.Next(int.MinValue, int.MaxValue);
+    }
+}
