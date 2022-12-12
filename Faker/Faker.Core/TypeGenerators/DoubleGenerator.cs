cedba6f7cb44c1c905f6207d4a4ccd558b47a558
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-12 20:06:40 +0300

    New feature: DoubleGenerator
    Added generator for creating random double values

---
+++ Faker/Faker.Core/TypeGenerators/DoubleGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Faker.Core.Interface;
+using Faker.Core.Other;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Core.TypeGenerators;
+
+public class DoubleGenerator : IValueGenerator
+{
+    private readonly double _max = 100.0;
+
+    private readonly double _min = 0.0;
+
+    public bool CanGenerate(Type type) => type == typeof(double);
+
+    public object Generate(Type typeToGenerate, GeneratorContext context)
+    {
+        return context.Random.NextDouble() * (_max - _min) + _min;
+    }
+}
