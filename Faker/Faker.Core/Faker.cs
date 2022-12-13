0d7acb4b5f84ca8be9b7e8d1ad55a75f00af6d16
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-13 09:34:58 +0300

    Update Faker.cs
    Added Faker constructor, list of available generators and context.
    Method T Create will cast to Create(Type t)

--- Faker/Faker.Core/Faker.cs
+++ Faker/Faker.Core/Faker.cs
@@ -1,16 +1,27 @@
 ﻿using Faker.Core.Interface;
+using Faker.Core.Other;
+using Faker.Core.TypeGenerators;
 
 namespace Faker.Core;
 
 public class Faker : IFaker
 {
-    public T Create<T>()
+    private readonly List<IValueGenerator> _valueGenerators;
+    private readonly GeneratorContext _context;
+
+    Faker()
     {
-        throw new NotImplementedException();
+        _valueGenerators = new List<IValueGenerator> {new BoolGenerator(), new ByteGenerator(),
+            new CharGenerator(), new DecimalGenerator(), new DoubleGenerator(), new FloatGenerator(),
+            new IntGenerator(), new ListGenerator(), new LongGenerator(), new PointGenerator(),
+            new ShortGenerator(), new StringGenerator()};
+        _context = new GeneratorContext(new Random(), this);
     }
+
+    public T Create<T>() => (T)Create(typeof(T));
 
     public object Create(Type t)
     {
-        throw new NotImplementedException();
+        
     }
 }
