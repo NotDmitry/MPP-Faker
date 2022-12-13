1b4b4c524afbdbd056d5ff21492a703b08b6db15
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-13 10:17:26 +0300

    Updates and new feature
    Added class NestedClassGuard for protection from cycle initialization of complex objects.
    
    Faker.cs is now using NestedClassGuard

--- Faker/Faker.Core/Faker.cs
+++ Faker/Faker.Core/Faker.cs
@@ -8,6 +8,7 @@ public class Faker : IFaker
 {
     private readonly List<IValueGenerator> _valueGenerators;
     private readonly GeneratorContext _context;
+    private readonly NestedClassGuard _classGuard;
 
     Faker()
     {
@@ -16,13 +17,21 @@ public class Faker : IFaker
             new IntGenerator(), new ListGenerator(), new LongGenerator(), new PointGenerator(),
             new ShortGenerator(), new StringGenerator()};
         _context = new GeneratorContext(new Random(), this);
+        _classGuard = new NestedClassGuard(4);
     }
 
     public T Create<T>() => (T)Create(typeof(T));
 
     public object Create(Type t)
     {
+        _classGuard.ChangeOccurance(t, NestedClassGuard.Operation.Add);
+        if (_classGuard.IsStuck(t))
+            return null;
+
         var valueGenerator = _valueGenerators.FirstOrDefault(g => g.CanGenerate(t), new ObjectGenerator());
+
+        _classGuard.ChangeOccurance(t, NestedClassGuard.Operation.Delete);
+
         return valueGenerator.Generate(t, _context);
         
     }
---
+++ Faker/Faker.Core/Other/NestedClassGuard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Core.Other;
+
+public class NestedClassGuard
+{
+	private int _maxLevel;
+	private Dictionary<Type, int> _nestedClasses;
+	public enum Operation
+	{
+		Add,
+		Delete
+	}
+
+	public NestedClassGuard(int maxLevel)
+	{
+		_nestedClasses = new Dictionary<Type, int>();
+		_maxLevel = maxLevel;
+	}
+
+	public void ChangeOccurance(Type type, Operation op)
+	{
+		switch (op)
+		{
+			case Operation.Add:
+				{
+					if (_nestedClasses.ContainsKey(type))
+						_nestedClasses[type]++;
+					else
+						_nestedClasses.Add(type, 1);
+				}
+				break;
+			case Operation.Delete:
+				_nestedClasses[type]--;
+				break;
+			default:
+				break;
+		}
+
+	}
+
+    public bool IsStuck(Type type) => _nestedClasses[type] > _maxLevel;
+
+}
